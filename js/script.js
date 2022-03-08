1d56c1adb18f54d391e7e3bcb071b63caee2f526
Promise Nwafor <makybeky9@gmail.com>
2022-03-08 22:05:15 +0100

    replace coming soon files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+$(".owl-carousel").owlCarousel({
+    autoplay: true,
+    loop: true,
+    items: 1,
+    // stagePadding: 10,
+});
+
+AOS.init({
+    debounceDelay: 50, 
+    throttleDelay: 99, 
+    offset: 120, 
+    delay: 0, 
+    duration: 400, 
+    easing: 'ease', 
+    once: true, 
+  });
+
